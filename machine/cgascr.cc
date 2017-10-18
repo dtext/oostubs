71671103cbc489718a5633a110bb436fd93dfa16
Unknown <julius.adamek@web.de>
2017-10-18 18:03:34 +0200

    implemented cgscreen

--- machine/cgascr.cc
+++ machine/cgascr.cc
@@ -13,8 +13,10 @@
 /*****************************************************************************/
 
 #include "machine/cgascr.h"
+#include "machine/io_port.h"
 
-void CGA_Screen::show (int x, int y, char c, unsigned char attrib) {
+/* Hier muesst ihr selbst Code vervollstaendigen */
+void CGA_Screen::show (int x, int y, char c, unsigned char attrib){
     char *CGA_START = (char *)0xb8000;
     char *pos;
     pos = CGA_START + 2*(x + y*80);
@@ -22,14 +24,36 @@ void CGA_Screen::show (int x, int y, char c, unsigned char attrib) {
     *(pos + 1) = attrib;
 }
 
-void CGA_Screen::setpos (int x, int y) {
+void CGA_Screen::setpos (int x, int y){
+    IO_Port index_port = IO_Port(0x3d4);
+    IO_Port data_port = IO_Port(0x3d5);
 
+    index_port.outb(14);
+    data_port.outb(x);
+
+    index_port.outb(15);
+    data_port.outb(y);
 }
 
-void CGA_Screen::getpos (int &x, int &y) {
+void CGA_Screen::getpos(int &x, int &y){
+    IO_Port index_port = IO_Port(0x3d4);
+    IO_Port data_port = IO_Port(0x3d5);
 
+    index_port.outb(14);
+    x = data_port.inb();
+
+    index_port.outb(15);
+    y = data_port.inb();
 }
 
-void CGA_Screen::print (char* text, int length, unsigned char attrib) {
-
+void CGA_Screen::print(char* text, int length, unsigned char attrib){
+    int x,y;
+    getpos(x,y);
+    //x=0;y=0;
+    for(int i=0;i<length;i++){
+        show(x,y,*text,attrib);
+        text++;
+        x++;
+    }
+    setpos(x,y);
 }
--- main.cc
+++ main.cc
@@ -1,8 +1,21 @@
 #include "machine/cgascr.h"
 
+/* Hier muesst ihr selbst Code vervollstaendigen */
+#include "machine/cgascr.h"
+
 int main()
 {
+
     CGA_Screen cga_screen;
-    cga_screen.show(0,0,'X',0x0f);
-    return 0;
+    cga_screen.setpos(0,0);
+    cga_screen.print("hallo",5,15);
+    cga_screen.print("hallo",5,15);
+
+/* Hier muesst ihr selbst Code vervollstaendigen */         
+         
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+                         
+/* Hier muesst ihr selbst Code vervollstaendigen */                         
+ 
+   return 0;
  }
