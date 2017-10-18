5c443be7a12968c9753739fbd4bf8b519b9066bb
dtext <textores.danny@gmail.com>
2017-10-18 16:51:20 +0200

    Setup project

---
+++ main.cc
@@ -0,0 +1,14 @@
+/* $Id$ */
+
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+        
+int main()
+{
+/* Hier muesst ihr selbst Code vervollstaendigen */         
+         
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+                         
+/* Hier muesst ihr selbst Code vervollstaendigen */                         
+ 
+   return 0;
+ }
