c45833e50c7127149131ab62240558593430549e
dtext <textores.danny@gmail.com>
2017-11-08 16:05:56 +0100

    Add demo application code for assignment completion

--- user/appl.cc
+++ user/appl.cc
@@ -19,19 +19,32 @@
 extern CGA_Stream kout;
 
 void Application::action() {
-    CGA_Stream stream;
+    CGA_Stream stream, kout;
     Keyboard_Controller keyboard;
 
-    char led_number = 0;
+    int i = 768;
+    int j = -480;
+
+    unsigned long l = (unsigned long) -1;
+
+    // print some numbers
+    kout << "Integer of value " << i
+         << " in hex: " << hex << i
+         << ", octal: " << oct << i
+         << " and binary: " << bin << i << '\n';
+
+    kout << "Negative Values work, too: " << dec << j << '\n';
+
+    kout << "Look! A very large number: " << l << '\n';
+    kout.flush();
+
+    // read stuff from keyboard
     Key myKey;
     while (true) {
         myKey = keyboard.key_hit();
         if(myKey.valid()) {
             stream << myKey.ascii();
             stream.flush();
-            keyboard.set_led(led_number,0);
-            led_number = led_number + 1 ? led_number < 2 : 0;
-            keyboard.set_led(led_number,1);
         }
     }
 }
