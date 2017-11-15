4e4862e4b67657d082cf8fc7af5a34f7b9206ffd
JulianSauer <julian_sauer@gmx.net>
2017-11-15 18:17:42 +0100

    Add blue screen

--- device/panic.cc
+++ device/panic.cc
@@ -12,14 +12,51 @@
 
 /* INCLUDES */
 
+#define MARGIN_LEFT 7
+#define MARGIN_RIGHT (80 - MARGIN_LEFT)
+
+
 #include "device/panic.h"
 #include "device/cgastr.h"
 #include "machine/cpu.h"
 
 Panic panic;
 
+int getErrorLength(char *error) {
+    int i;
+    for (i = 0; error[i]; i++);
+    return i;
+}
+
 bool Panic::prologue() {
-    cout << "Kernel Panic :(" << flush;
+    for (int i = 0; i < 2000; i++)
+        cout.show(i, 0, ' ', 1 << 4);
+    int marginTop = 3;
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print("      __", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print(" _   / /", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print("(_) | | ", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print(" _  | | ", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print("(_) | | ", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop++);
+    cout.print("     \\_\\", 9, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, marginTop += 2);
+    cout.print("Kernel Panic!", 15, 1 << 4 | 15);
+    cout.setpos(MARGIN_LEFT, ++marginTop);
+
+    // Prints error message
+    int x, y;
+    for (int i = 0; error[i]; i++) {
+        if (i != 0 && !(i % MARGIN_RIGHT))
+            cout.setpos(MARGIN_LEFT, ++marginTop);
+        cout.getpos(x, y);
+        cout.show(x, y, error[i], 1 << 4 | 15);
+        cout.setpos(++x, y);
+    }
     cpu.halt();
     return false; // this is here to avoid compiler errors
 }
--- device/panic.h
+++ device/panic.h
@@ -18,10 +18,15 @@
 class Panic : public Gate {
 private:
     Panic(const Panic &copy); // Verhindere Kopieren
+    char* error;
 public:
     Panic() {}
 
     bool prologue();
+
+    void setError(char* error) {
+        this->error = error;
+    }
 };
 
 extern Panic panic;
