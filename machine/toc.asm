44654ce0c98e33752d8da69c974d83f7a58aaf38
Florian Lüdiger <florian.luediger@googlemail.com>
2017-11-22 16:30:29 +0100

    Add guideline for the fourth exercise

---
+++ machine/toc.asm
@@ -0,0 +1,40 @@
+; $Id$
+
+;******************************************************************************
+;* Betriebssysteme                                                            *
+;*----------------------------------------------------------------------------*
+;*                                                                            *
+;*                               T O C . A S M                                *
+;*                                                                            *
+;*----------------------------------------------------------------------------*
+;*
+;******************************************************************************
+
+%include "machine/toc.inc"
+
+; EXPORTIERTE FUNKTIONEN
+
+[GLOBAL toc_switch]
+[GLOBAL toc_go]
+
+; IMPLEMENTIERUNG DER FUNKTIONEN
+
+[SECTION .text]
+
+; TOC_GO : Startet den ersten Prozess ueberhaupt.
+;
+; C Prototyp: void toc_go (struct toc* regs, void *coroutine);
+
+toc_go:
+; Hier muesst ihr selbst Code vervollstaendigen 
+
+; TOC_SWITCH : Prozessumschaltung. Der aktuelle Registersatz wird
+;              gesichert und der Registersatz des neuen "thread of control"
+;              wird in den Prozessor eingelesen.
+;
+; C Prototyp: void toc_switch (struct toc* regs_now,
+;                              struct toc* reg_then,
+;                              void *coroutine);
+
+toc_switch:
+; Hier muesst ihr selbst Code vervollstaendigen 
---
+++ machine/toc.c
@@ -0,0 +1,22 @@
+/* $Id$ */
+
+/*****************************************************************************/
+/* Betriebssysteme                                                           */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                                 T O C                                     */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Die Struktur toc dient dazu, bei einem Koroutinenwechsel die Werte der    */
+/* nicht-fluechtigen Register zu sichern. toc_settle bereitet sie            */
+/* entsprechend vor.                                                         */
+/*****************************************************************************/
+
+#include "machine/toc.h"
+
+// TOC_SETTLE: bereitet den Kontext der Koroutine fuer den ersten
+//             Aufruf vor.
+void toc_settle (struct toc* regs, void* tos, void (*kickoff)(void*))
+ {
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+ }
