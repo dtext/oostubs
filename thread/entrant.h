0f9d64b8ec290015a13d44cb6ef0bc6efae6f242
dtext <textores.danny@gmail.com>
2017-11-29 18:25:27 +0100

    Implement Entrant class

--- thread/entrant.h
+++ thread/entrant.h
@@ -11,14 +11,14 @@
 #ifndef __entrant_include__
 #define __entrant_include__
 
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
-        
-class Entrant 
-/* Hier muesst ihr selbst Code vervollstaendigen */         
- {
+#include "thread/coroutine.h"
+#include "object/chain.h"
+
+class Entrant : public Coroutine, public Chain {
 private:
-      Entrant (const Entrant &copy); // Verhindere Kopieren
-/* Hier muesst ihr selbst Code vervollstaendigen */          
- };
+    Entrant(const Entrant &copy); // Verhindere Kopieren
+public:
+    Entrant(void *tos) : Coroutine(tos) {};
+};
 
 #endif
