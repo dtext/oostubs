7df6a023fd896c3a8bcfc88a03808e93b7a43b83
dtext <textores.danny@gmail.com>
2017-12-06 17:00:51 +0100

    Move documentation to header file

--- thread/dispatch.h
+++ thread/dispatch.h
@@ -17,6 +17,10 @@
 
 #include "thread/coroutine.h"
 
+/**
+ * Dispatcher manages the pointer to the currently active coroutine and
+ * executes the context switches between coroutines.
+ */
 class Dispatcher {
 private:
     Dispatcher(const Dispatcher &copy); // Verhindere Kopieren
@@ -29,10 +33,21 @@ public:
         activeCoroutine = 0;
     }
 
+    /**
+     * Makes first the active coroutine and calls its go() function
+     * @param first the first coroutine in the system
+     */
     void go(Coroutine &first);
 
+    /**
+     * Switches from the active coroutine to the next coroutine.
+     * @param next the coroutine to switch to
+     */
     void dispatch(Coroutine &next);
 
+    /**
+     * @return the currently active coroutine
+     */
     Coroutine *active() {
         return activeCoroutine;
     }
