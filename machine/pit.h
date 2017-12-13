23b467707fcf490e435e12cb1c86021d9c447f14
Florian Lüdiger <florian.luediger@googlemail.com>
2017-12-13 17:09:26 +0100

    WIP: Implement PIT

--- machine/pit.h
+++ machine/pit.h
@@ -11,17 +11,21 @@
 #ifndef __pit_include__
 #define __pit_include__
 
+#include "io_port.h"
+
 class PIT {
 private:
     PIT(const PIT &copy); // Verhindere Kopieren
-/* Hier muesst ihr selbst Code vervollstaendigen */
+    int interval_us;
+    IO_Port control_port = IO_Port(0x43);
+    IO_Port data_port = IO_Port(0x40);
 public:
     PIT(int us) {
         interval(us);
     }
 
     int interval() {
-/* Hier muesst ihr selbst Code vervollstaendigen */
+        return interval_us;
     }
 
     void interval(int us);
