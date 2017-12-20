1cfcc96374ecb2364daeb0f3a4beabb1b173eb1e
Julius Adamek <julius.adamek@web.de>
2017-12-20 17:12:15 +0100

    fix pit

--- machine/pit.cc
+++ machine/pit.cc
@@ -10,13 +10,12 @@
 
 #include "pit.h"
 
-/* Hier muesst ihr selbst Code vervollstaendigen */
 void PIT::interval(int us) {
     interval_us = us;
     // 00:Counter 0; 11:lower then higher byte; 010:periodic interrupts; 0:binary counting
     control_port.outb(0b00110100);
     char lower = us;
-    char higher = us << 8;
+    char higher = us >> 8;
     data_port.outb(lower);
     data_port.outb(higher);
 }
