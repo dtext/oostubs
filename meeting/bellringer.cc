e760ea4516ba3c0bcc40a2d2e9edfce2f7fd9795
dtext <textores.danny@gmail.com>
2018-01-17 17:52:23 +0100

    Implement Bellringer class

--- meeting/bellringer.cc
+++ meeting/bellringer.cc
@@ -10,5 +10,51 @@
 /* Glocken befinden sich in einer Queue, die der Gloeckner verwaltet.        */
 /*****************************************************************************/
 
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
+#include "bellringer.h"
 
+void Bellringer::check() {
+    auto *bell = (Bell *) List::head;
+    // tick first bell upon check
+    if (bell != nullptr) {
+        bell->tick();
+    }
+    // ring all bells that are run down
+    while (bell && bell->run_down()) {
+        bell->ring();
+        bell = (Bell *) bell->next;
+    }
+    // remove all bells that were rung
+    List::head = bell;
+    if (List::head == nullptr)
+        List::tail = &head;
+}
+
+void Bellringer::job(Bell *bell, int ticks) {
+    int tick_count = 0;
+    // find list position for new job
+    auto *listed = (Bell *) List::head;
+    while (listed && tick_count + listed->wait() < ticks) {
+        tick_count += listed->wait();
+        listed = (Bell *) listed->next;
+    }
+    // delta encode wait time
+    bell->wait(ticks - tick_count);
+
+    // if new job is first bell to be rung, insert at beginning
+    if (listed == nullptr) {
+        insert_first(bell);
+        return;
+    }
+    // otherwise insert at position
+    insert_after(listed, bell);
+}
+
+void Bellringer::cancel(Bell *bell) {
+    auto *next = (Bell *) bell->next;
+    // if theres another item in the queue, add this job's timer value to theirs
+    if (next != nullptr) {
+        next->wait(next->wait() + bell->wait());
+    }
+    // then just remove the job
+    Queue::remove(bell);
+}
