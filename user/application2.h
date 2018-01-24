61fb11a1d665e0cb19afc6aabc6a6407b71b3e47
dtext <textores.danny@gmail.com>
2018-01-24 17:17:21 +0100

    Improve output in application2

--- user/application2.h
+++ user/application2.h
@@ -6,31 +6,40 @@
 #include <guard/secure.h>
 
 class MyJob : public Bell {
+private:
+    char *name;
 public:
+    MyJob() {}
+
+    void setName(char *name) {
+        this->name = name;
+    }
+
     void ring() {
-        cout << "Ringading" << flush;
+        cout << "Job \"" << name << "\" has finished." << endl << flush;
     }
 };
 
 class Application2 : public Thread {
 public:
     Application2(void *tos) : Thread(tos) {}
-    MyJob job;
+
+    MyJob job1;
     MyJob job2;
     MyJob job3;
     MyJob job4;
 
     void action() override {
+        job1.setName("#1");
+        job2.setName("#2");
+        job3.setName("#3");
+        job4.setName("#4");
         {
             Secure secure;
-            bellringer.job(&job, 10000);
-            bellringer.dump();
+            bellringer.job(&job1, 5000);
             bellringer.job(&job2, 10000);
-            bellringer.dump();
-            bellringer.job(&job3, 10000);
-            bellringer.dump();
-            bellringer.job(&job4, 10000);
-            bellringer.dump();
+            bellringer.job(&job3, 20000);
+            bellringer.job(&job4, 15000);
         }
         coutSemaphore.wait();
         cout << "Bar" << flush;
